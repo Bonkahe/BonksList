62d8abb895184b8b0c4ca99bb378638582e66ea3
David House <Bonkahe@gmail.com>
2021-02-16 14:52:00 -0600

    Continued work on cleaning up and learning the entity framework.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace BonksList.Controllers
         {
             return View();
         }
+        public IActionResult Listings()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
