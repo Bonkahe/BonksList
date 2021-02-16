b97ad00aac8b9ecf2f062dfb6c3ecc7a06ba1253
David House <Bonkahe@gmail.com>
2021-02-16 12:46:19 -0600

    Reworking a few things.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BonksList.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
